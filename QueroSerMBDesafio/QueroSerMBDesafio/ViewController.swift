a8cbcc4535ef7f0fd1627e3659c64da6a64ff215
Felipe Perius <felipeperius@MacBook-Pro-de-Felipe.local>
2020-11-05 14:15:31 -0300

    start project

---
+++ QueroSerMBDesafio/QueroSerMBDesafio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QueroSerMBDesafio
+//
+//  Created by Felipe Perius on 05/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
